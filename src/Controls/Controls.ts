8aa5b6617ad2121186b4ed6ff85d153eaa1f8567
Oleg Koryakov <outflowpwnz@gmail.com>
2020-11-14 13:41:23 +0700

    added controls

---
+++ src/Controls/Controls.ts
@@ -0,0 +1,36 @@
+import { IApp, IOptions } from '../App/types';
+
+export default class Controls {
+  constructor(controlsElement: HTMLElement, app: IApp) {
+    this.controlsElement = controlsElement;
+    this.app = app;
+    this.init();
+  }
+
+  private controlsElement: HTMLElement;
+
+  private app: IApp;
+
+  private init() {
+    const applyButton = this.controlsElement.querySelector('.controls__apply-button') as HTMLButtonElement;
+    applyButton.addEventListener('click', this.onApplyButtonClick);
+  }
+
+  private onApplyButtonClick = () => {
+    const inputs = this.controlsElement.querySelectorAll('.controls__control-input');
+    const options: any = {};
+    inputs.forEach((element) => {
+      const input = element as HTMLInputElement;
+      const { type, id } = input;
+      if (type === 'range') {
+        const value = parseInt(input.value, 10);
+        options[id] = value;
+      } else if (type === 'checkbox') {
+        const { checked } = input;
+        options[id] = checked;
+      }
+    });
+
+    this.app.transform(options as IOptions);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import './styles.scss';
-import View from './App/App';
+import App from './App/App';
+import Controls from './Controls/Controls';
 
-const root = document.querySelector('#root') as HTMLElement;
-
+const root = document.querySelector('.affine-transformation__graph') as HTMLElement;
+const controlsElement = document.querySelector('.controls') as HTMLElement;
 // eslint-disable-next-line no-unused-vars
-const canvas = new View({}, root);
-// canvas.drawSmthng();
+const app = new App({}, root);
+// eslint-disable-next-line no-unused-vars
+const controls = new Controls(controlsElement, app);
